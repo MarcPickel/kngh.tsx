cc1474a129cfe853dd530a6dd6e8a12b8dba2a8b
Marc Pickelman <marcpc@att.net>
2026-08-12 16:11:21 -0500

    Add folders and files to features

---
+++ src/features/Main/Main.tsx
@@ -0,0 +1,28 @@
+function Main() {
+    return (
+    <main className="main">
+
+{/*
+
+What ought to be in the form?
+- The form is for prayer intentions for the conversion of sinners.
+- So, a name is given of the person in need of prayer.
+- Perhaps the affiliate of the named person? e.g. Mother, Father, Brother, Sister, Friend, etc.
+- A text box that allows the person to give any detail that might be relevant, or anything that person is struggling with
+for which we could pray for specifically (but would that be prudent and holy? It might just be better to ask for a name without description)
+
+Then, upon submission, the form ought to say:
+- Thank you for submitting your loved one's name (or whatever)
+- (Loved one's name) has been added into our prayers for the conversion of sinners,
+as well as for our sacrifices and offering Holy Communion for the conversion of sinners
+
+*/}
+
+        <form>
+
+        </form>
+
+    </main>)
+}
+
+export default Main;
